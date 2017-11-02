c682f00c240b4ff0e5aced914eff5f248a73510c
michelle <michelle@T60-01>
2017-11-02 15:35:53 +0900

    add moving Car with KeyEvents

--- src/hello_world/Car.java
+++ src/hello_world/Car.java
@@ -1,6 +1,9 @@
 package hello_world;
 
-public class Car extends Vehicle {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Car extends Vehicle implements KeyListener {
     public Car(int x, int y, int vx, int vy) {
         super(x, y, vx, vy);
     }
@@ -9,5 +12,34 @@ public class Car extends Vehicle {
         frame.fillRect(x, y + 10, 40, 10);
         frame.fillOval(x + 5, y + 20, 10, 10);
         frame.fillOval(x + 25, y + 20, 10, 10);
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            vx -= 5;
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            vx += 5;
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            vy -= 5;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            vy += 5;
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            vx = 0;
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            vx = 0;
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            vy = 0;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            vy = 0;
+        }
     }
 }
--- src/hello_world/PlayerAnimation.java
+++ src/hello_world/PlayerAnimation.java
@@ -3,6 +3,7 @@ package hello_world;
 public class PlayerAnimation extends MyFrame {
     public void run() {
         Car player = new Car(100, 200, 0, 0);
+        addKeyListener(player);
         while (true) {
             clear();
             player.draw(this);
